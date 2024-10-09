aad7724d26464c8fca3c35ec2c517139cf9240f5
gokulvadivel99 <125590447+gokulvadivel99@users.noreply.github.com>
2024-10-09 16:27:09 +0530

    Update FirstWebDriver.java

--- FirstWebDriver.java
+++ FirstWebDriver.java
@@ -1,7 +1,7 @@
 package SeleniumDay1;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.WebDriver;
 public class FirstWebDriver {
 
 	public static void main(String[] args) {
